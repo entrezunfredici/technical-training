c305989a79e45daa0b5d9451659f84a8164cdbee
entrezunfredici <superfred2468@gmail.com>
2023-06-16 10:32:57 +0200

    Test

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,11 +1,17 @@
 {
-    "name": "Estate",  # The name that will appear in the App list
-    "version": "16.0",  # Version
-    "application": True,  # This line says the module is an App, and not a module
+    'name': "Real_Estate",
+    'version': '16.0',
+    'depends': ['base'],
+    'author': "Macabiau Frédéric",
+    'category': 'estate',
+    'description': """
+    Description text
+    """,
     "depends": ["base"],  # dependencies
     "data": [
 
     ],
+    "application": True,  # This line says the module is an App, and not a module
     "installable": True,
     'license': 'LGPL-3',
 }
